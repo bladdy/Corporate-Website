585a0366e661e99d3b41c4f01e0eed99c2a63830
Bladimir Almanzar <bladdy34@gmail.com>
2026-03-06 16:42:12 -0600

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone"
-  }),
+  //adapter: node({ mode: 'standalone' }), // Use this for docker production with Node.js
+  adapter: vercel(), // Use this for deployment on Vercel
   trailingSlash: 'always',
   i18n: {
     locales: ['es', 'en'],
