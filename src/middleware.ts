25aab7d57bba12f520f3df63b8a0b3a7ca9cd60a
Bladimir Almanzar <bladdy34@gmail.com>
2026-03-07 20:36:14 -0600

    Refactor middleware to improve language handling and redirect logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,18 @@ import { defineMiddleware } from "astro:middleware";
 import { languages, defaultLang } from "./i18n/config";
 
 export const onRequest = defineMiddleware((context, next) => {
-  const lang = context.url.pathname.split("/")[1];
+
+  const { pathname } = context.url;
+
+  const segments = pathname.split("/");
+  const lang = segments[1];
 
   if (!languages[lang as keyof typeof languages]) {
-    return context.redirect(`/${defaultLang}/${context.url.pathname}`);
+
+    return context.redirect(`/${defaultLang}${pathname}`);
+
   }
 
   return next();
+
 });
